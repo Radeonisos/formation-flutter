4bd5cfb24173787cc697c9de6efc89e89e4cd987
Radeonisos <fournierdavid49@yahoo.fr>
2020-03-07 21:32:08 +0100

    no message

--- flutter_first_app/lib/main.dart
+++ flutter_first_app/lib/main.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_first_app/questions.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new _MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  var _index = 0;
+
+  void _answerQuestion() {
+    print("Answer chosen!");
+    setState(() {
+      if (_index < 1) _index = _index + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "Quel est ta couleur préferée?",
+      "Quel est ton animal fétiche?"
+    ];
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -17,7 +36,23 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("My First App"),
         ),
-        body: Text('This is my default text!'),
+        body: Column(
+          children: [
+            Question(questions[_index]),
+            RaisedButton(
+              child: Text("Réponse 1"),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text("Réponse 2"),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text("Réponse 3"),
+              onPressed: _answerQuestion,
+            ),
+          ],
+        ),
       ),
     );
   }
